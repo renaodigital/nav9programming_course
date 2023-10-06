8fda4502583b4dc61d955c1a2cc6b8c159fbc226
Renão <78944713+renaodigital@users.noreply.github.com>
2023-10-05 23:01:13 -0300

    uptoDate

---
+++ blocoUm/prática/parteUm/exercicioOito.js
@@ -0,0 +1,28 @@
+// 08 - Calcule a média de consumo de combustível utilizada em uma viagem e o
+// tempo levado durante a viagem. Você deve imprimir no console esses dois
+// valores:
+
+// Os valores de entrada serão:
+// Distância percorrida na viagem em km;
+// Velocidade do automóvel em km/h;
+// Quantidade de combustível gasto em litros
+
+// Questão = O que eu quero: Consumo de Combustível
+// Meio = O que preciso pra isso: Saber quanto o carro faz por litro
+
+// Distância = 200 km
+// Velocidade 90 km/h
+// Quantidade 20 litros
+
+
+let distanciaKm = 200;
+let velocidadeKmH = 90;
+let quantidadeGastaLitros = 20;
+
+console.log("A viagem durou: " + distanciaKm/velocidadeKmH + "h");
+
+let quantosKmL = 5;
+
+console.log("Foram consumidos: " + quantidadeGastaLitros/quantosKmL);
+
+// Done
--- blocoUm/prática/parteUm/exercicioSete.js
+++ blocoUm/prática/parteUm/exercicioSete.js
@@ -1,3 +1,16 @@
 // 07 - Tendo como informação dois valores, valor1 e valor2, faça um programa
 // que imprima os dois valores no console. Após imprimir, troque a informação de
 // valor1 pela informação do valor2 e imprima no console novamente.
+
+let value1 = 32;
+let value2 = 33;
+let showvalues = "Valor 1: " + value1 + " " + "Valor 2: " + value2;
+
+console.log(showvalues);
+
+value1 = 33;
+value2 = 32;
+showvalues = "Valor 1: " + value1 + " " + "Valor 2: " + value2;
+console.log(showvalues);
+
+// Done

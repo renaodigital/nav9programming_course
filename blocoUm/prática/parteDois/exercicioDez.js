497e46821c5171f1cb5b469bac295b44bb535179
Renão <78944713+renaodigital@users.noreply.github.com>
2023-10-05 23:33:59 -0300

    uptoDate

--- blocoUm/prática/parteDois/exercicioDez.js
+++ blocoUm/prática/parteDois/exercicioDez.js
@@ -1,3 +1,13 @@
 //  10 - Tendo como informação dois números, faça um programa que imprima na
 //  tela qual deles é o MAIOR e qual deles é o menor. Caso os números sejam
 //  iguais, informe que eles são iguais.
+
+let numberOne = 25;
+let numberTwo = 36;
+
+if(numberTwo>numberOne) {
+    console.log("36 é maior que 25");
+}
+
+// Done
+
